0c48288f74f802279a0cc5f0e7b1a36350e5f8bb
zhaoyueheng <112373131+zhaoyueheng@users.noreply.github.com>
2025-05-15 03:51:17 +0100

    Add files via upload

---
+++ src/data/generated/bib.js
@@ -0,0 +1,114 @@
+﻿const generatedBibEntries = {
+    "audinys2025deep": {
+        "author": "Audinys, Robertas and {\\v{S}}likas, {\\v{Z}}ygimantas and Radkevi{\\v{c}}ius, Justas and {\\v{S}}utas, Mantas and Ostreika, Armantas",
+        "evaluationmethod": "End-to-End #7",
+        "journal": "Electronics",
+        "number": "5",
+        "pages": "825",
+        "publisher": "MDPI",
+        "title": "Deep Reinforcement Learning for a Self-Driving Vehicle Operating Solely on Visual Information",
+        "type": "article",
+        "volume": "14",
+        "year": "2025"
+    },
+    "baheri2020deep": {
+        "author": "Baheri, Ali and Nageshrao, Subramanya and Tseng, H Eric and Kolmanovsky, Ilya and Girard, Anouck and Filev, Dimitar",
+        "booktitle": "2020 IEEE Intelligent Vehicles Symposium (IV)",
+        "evaluationmethod": "Safety #3",
+        "organization": "IEEE",
+        "pages": "1550--1555",
+        "title": "Deep reinforcement learning with enhanced safety for autonomous highway driving",
+        "type": "inproceedings",
+        "year": "2020"
+    },
+    "feng2023dense": {
+        "author": "Feng, Shuo and Sun, Haowei and Yan, Xintao and Zhu, Haojie and Zou, Zhengxia and Shen, Shengyin and Liu, Henry X",
+        "evaluationmethod": "Safety #5",
+        "journal": "Nature",
+        "number": "7953",
+        "pages": "620--627",
+        "publisher": "Nature Publishing Group UK London",
+        "title": "Dense reinforcement learning for safety validation of autonomous vehicles",
+        "type": "article",
+        "volume": "615",
+        "year": "2023"
+    },
+    "khlifi2025novel": {
+        "author": "Khlifi, Ahmed and Othmani, Mohamed and Kherallah, Monji",
+        "evaluationmethod": "Sample Efficiency #6",
+        "journal": "World Electric Vehicle Journal",
+        "number": "3",
+        "pages": "138",
+        "publisher": "MDPI",
+        "title": "A Novel Approach to Autonomous Driving Using Double Deep Q-Network-Bsed Deep Reinforcement Learning",
+        "type": "article",
+        "volume": "16",
+        "year": "2025"
+    },
+    "kiran2021deep": {
+        "author": "Kiran, B Ravi and Sobh, Ibrahim and Talpaert, Victor and Mannion, Patrick and Al Sallab, Ahmad A and Yogamani, Senthil and P{\\'e}rez, Patrick",
+        "evaluationmethod": "Literature Review #2",
+        "journal": "IEEE transactions on intelligent transportation systems",
+        "number": "6",
+        "pages": "4909--4926",
+        "publisher": "IEEE",
+        "title": "Deep reinforcement learning for autonomous driving: A survey",
+        "type": "article",
+        "volume": "23",
+        "year": "2021"
+    },
+    "li2024platform": {
+        "author": "Li, Dianzhao and Okhrin, Ostap",
+        "evaluationmethod": "Sim2Real #4",
+        "journal": "Communications Engineering",
+        "number": "1",
+        "pages": "147",
+        "publisher": "Nature Publishing Group UK London",
+        "title": "A platform-agnostic deep reinforcement learning framework for effective sim2real transfer towards autonomous driving",
+        "type": "article",
+        "volume": "3",
+        "year": "2024"
+    },
+    "liu2022improved": {
+        "author": "Liu, Haochen and Huang, Zhiyu and Wu, Jingda and Lv, Chen",
+        "booktitle": "2022 IEEE intelligent vehicles symposium (IV)",
+        "evaluationmethod": "Sample Efficiency #9",
+        "organization": "IEEE",
+        "pages": "921--928",
+        "title": "Improved deep reinforcement learning with expert demonstrations for urban autonomous driving",
+        "type": "inproceedings",
+        "year": "2022"
+    },
+    "ma2021reinforcement": {
+        "author": "Ma, Xiaobai and Li, Jiachen and Kochenderfer, Mykel J and Isele, David and Fujimura, Kikuo",
+        "booktitle": "2021 IEEE International Conference on Robotics and Automation (ICRA)",
+        "evaluationmethod": "Sim2Real",
+        "organization": "IEEE",
+        "pages": "6064--6071",
+        "title": "Reinforcement learning for autonomous driving with latent state inference and spatial-temporal relationships",
+        "type": "inproceedings",
+        "year": "2021"
+    },
+    "wang2024deep": {
+        "author": "Wang, Tao and Qu, Dayi and Wang, Kedong and Dai, Shouchen",
+        "evaluationmethod": "End-to-End #10",
+        "journal": "Electronics",
+        "number": "6",
+        "pages": "1133",
+        "publisher": "MDPI",
+        "title": "Deep Reinforcement Learning Car-Following Control Based on Multivehicle Motion Prediction",
+        "type": "article",
+        "volume": "13",
+        "year": "2024"
+    },
+    "xiang2024reinforcement": {
+        "author": "Xiang, Dantong",
+        "evaluationmethod": "Safety #8",
+        "journal": "Applied and Computational Engineering",
+        "pages": "17--23",
+        "title": "Reinforcement learning in autonomous driving",
+        "type": "article",
+        "volume": "48",
+        "year": "2024"
+    }
+};
